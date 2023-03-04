c2652cffb36aaa2b9551c623151aaa3f89e6374b
김민기 <mnii5625@gmail.com>
2023-03-04 15:14:42 +0900

    Initial commit

---
+++ src/main/java/auth/auth_setting/AuthSettingApplication.java
@@ -0,0 +1,13 @@
+package auth.auth_setting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSettingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthSettingApplication.class, args);
+    }
+
+}
